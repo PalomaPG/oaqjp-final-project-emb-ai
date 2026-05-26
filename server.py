acf84dd8e347f82b48b7790aed30e1d4ebad3dd3
palomapg <paloma.perez.gar@gmail.com>
2026-05-25 21:40:06 -0400

    updating server.py

--- server.py
+++ server.py
@@ -1,2 +1,19 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
+
+# app initialization
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def detect_emotion():
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+    return response
+
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
