0c791ff5b098ada330017533c9c1a0cbd67f7829
palomapg <paloma.perez.gar@gmail.com>
2026-05-26 18:50:38 -0400

    pylint score 10 in server.py

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,18 +10,28 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows a descriptive text
+        explaining the result and the dominant emotion.
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
     fmt_response = (
-        f"For the given statement, the system response is 'anger': {response["anger"]}, " 
-        f"'disgust': {response["disgust"]}, 'fear': {response["fear"]}, 'joy': {response["joy"]} "
-        f"and 'sadness': {response["sadness"]}. The dominant emotion is <b>{response["dominant_emotion"]}</b>"
+        f"For the given statement, the system response is 'anger': {response['anger']}, " 
+        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} "
+        f"and 'sadness': {response['sadness']}. The dominant emotion is " 
+        f"<b>{response['dominant_emotion']}.</b>"
     )
     return fmt_response
 
 
 @app.route("/")
 def render_index_page():
+    ''' 
+        This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
