78ccd0b49f54047236a4f4d834d2adde09e4e31f
palomapg <paloma.perez.gar@gmail.com>
2026-05-25 21:05:27 -0400

    more unit tests

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -14,7 +14,13 @@ class TestEmotionDetection(unittest.TestCase):
         response = emotion_detector("I feel disgusted just hearing about this")
         self.assertEqual(response["dominant_emotion"], "disgust")             
     
-    
+    def test_sadness(self):
+        response = emotion_detector("I am so sad about this")
+        self.assertEqual(response["dominant_emotion"], "sadness")
+
+    def test_joy(self):
+        response = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(response["dominant_emotion"], "fear")
 
 if __name__ == "__main__":
     unittest.main()
