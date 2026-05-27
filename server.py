c870c4059308b4dd6b37cc2ae5b7e7e49ea1603c
Paloma <paloma.perez.gar@gmail.com>
2026-05-27 18:18:56 -0400

    server managing 400 response from server

--- server.py
+++ server.py
@@ -17,6 +17,13 @@ def detect_emotion():
     '''
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
+    
+    if type(response) == str:
+        return response
+    else: # is a dictionary
+        if response["dominant_emotion"] == None:
+            return "Invalid text! Please try again!"
+    
     fmt_response = (
         f"For the given statement, the system response is 'anger': {response['anger']}, " 
         f"'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} "
@@ -24,7 +31,6 @@ def detect_emotion():
         f"<b>{response['dominant_emotion']}.</b>"
     )
     return fmt_response
-
 
 @app.route("/")
 def render_index_page():
